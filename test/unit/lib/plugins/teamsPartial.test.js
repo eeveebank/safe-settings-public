f50068e648cc2f2df94c0436a21c0f779506a9b8
Stephen Collings <stephen@mettle.co.uk>
2023-11-23 15:01:24 +0000

    Revert "tmp: break unit test"
    This reverts commit 1262a55e920b018286f2452b3ee9d30b81c89c9b.

--- test/unit/lib/plugins/teamsPartial.test.js
+++ test/unit/lib/plugins/teamsPartial.test.js
@@ -71,7 +71,7 @@ describe('TeamsPartial', () => {
         }
       )
 
-      expect(github.teams.addOrUpdateRepoPermissionsInOrg).not.toHaveBeenCalledWith({
+      expect(github.teams.addOrUpdateRepoPermissionsInOrg).toHaveBeenCalledWith({
         org,
         team_id: addedTeamId,
         team_slug: addedTeamName,
